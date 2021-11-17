19aa8227aea01cac71d36739d5a596efebce7ad7
Songchangyeop <waitwait0301@naver.com>
2021-11-17 18:39:32 +0900

    feat: Data를 axios로 받아오는 get-post-data 생성

---
+++ src/Common/get-post-data.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const getPostData = async (id) => {
+  try {
+    const response = await axios(
+      `https://limitless-sierra-67996.herokuapp.com/v1/posts${id}`,
+    );
+    console.log(response);
+    return response;
+  } catch (error) {
+    throw new Error('data load 실패');
+  }
+};
