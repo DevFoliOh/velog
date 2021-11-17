9a468cb8ea7f925ecd122f68725c25f91980a631
Yena-Yun <cheryleduart@gmail.com>
2021-11-17 17:48:54 +0900

    feat: ListPage 라우터 추가

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ListPage from 'Pages/ListPage/ListPage';
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" />
+        <Route path="/" component={ListPage} />
       </Switch>
     </Router>
   );
