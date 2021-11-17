46c07c2c331efc09b9741b32970a092d56d39340
Songchangyeop <waitwait0301@naver.com>
2021-11-17 17:48:54 +0900

    feat: Detail Title 컴포넌트 생성

---
+++ src/Components/DetailTitle/DetailTitle.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import * as style from './style';
+const DetailTitle = ({ title }) => <style.Title>{title}</style.Title>;
+
+export default DetailTitle;
---
+++ src/Components/DetailTitle/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size: 3rem;
+  line-height: 1.5;
+  letter-spacing: -0.004em;
+  margin-top: 0px;
+  font-weight: 800;
+  color: rgb(52, 58, 64);
+  margin-bottom: 2rem;
+  word-break: keep-all;
+`;
