a60fb704ffeda6d55aadf575ed0a7af8beb482c1
Songchangyeop <waitwait0301@naver.com>
2021-11-17 17:47:03 +0900

    feat: DetailPage에 Title 추가

--- src/Pages/DetailPage/DetailPage.jsx
+++ src/Pages/DetailPage/DetailPage.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import Header from 'Components/Header/Header';
+import DetailTitle from 'Components/DetailTitle/DetailTitle';
+import * as style from './style';
 
 const DetailPage = (props) => {
-  return <div>detail</div>;
+  return (
+    <div>
+      <Header></Header>
+      <style.Main>
+        <DetailTitle title="asdf"></DetailTitle>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex impedit
+        nesciunt adipisci enim consequuntur quibusdam voluptatem ut dicta
+        possimus nam inventore porro quis voluptas consequatur modi, iusto
+        pariatur. Eaque, culpa. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Ex impedit nesciunt adipisci enim consequuntur
+        quibusdam voluptatem ut dicta possimus nam inventore porro quis voluptas
+        consequatur modi, iusto pariatur. Eaque, culpa. Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Ex impedit nesciunt adipisci enim
+        consequuntur quibusdam voluptatem ut dicta possimus nam inventore porro
+        quis voluptas consequatur modi, iusto pariatur. Eaque, culpa.Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Ex impedit nesciunt
+        adipisci enim consequuntur quibusdam voluptatem ut dicta possimus nam
+        inventore porro quis voluptas consequatur modi, iusto pariatur. Eaque,
+        culpa.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+        impedit nesciunt adipisci enim consequuntur quibusdam voluptatem ut
+        dicta possimus nam inventore porro quis voluptas consequatur modi, iusto
+        pariatur. Eaque, culpa.Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Ex impedit nesciunt adipisci enim consequuntur
+        quibusdam voluptatem ut dicta possimus nam inventore porro quis voluptas
+        consequatur modi, iusto pariatur. Eaque, culpa.Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Ex impedit nesciunt adipisci enim
+        consequuntur quibusdam voluptatem ut dicta possimus nam inventore porro
+        quis voluptas consequatur modi, iusto pariatur. Eaque, culpa.Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Ex impedit nesciunt
+        adipisci enim consequuntur quibusdam voluptatem ut dicta possimus nam
+        inventore porro quis voluptas consequatur modi, iusto pariatur. Eaque,
+        culpa.
+      </style.Main>
+    </div>
+  );
 };
 
 export default DetailPage;
