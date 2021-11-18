658d4755f70f01166f9f9fb2fb44552eab93250f
Songchangyeop <waitwait0301@naver.com>
2021-11-19 00:29:26 +0900

    rename: useGetDetailData hook을 useGetData 로 변경

---
+++ src/Hooks/useGetData.js
@@ -0,0 +1,34 @@
+import { getCommentData, getPostData } from 'Common/api';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
+const useGetData = (setPostData, setComment) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [postId, setPostId] = useState();
+  const Id = useSelector((state) => state.getCardIdReducer.cardId);
+
+  useEffect(() => {
+    Id && setPostId(Id);
+  }, [Id]);
+
+  const getData = async () => {
+    try {
+      setIsLoading(true);
+      const postResponse = await getPostData(postId);
+      // const commentResponse = await getCommentData(postId);
+      setPostData(postResponse.data);
+      // setComment(commentResponse.data);
+      setIsLoading(false);
+    } catch (error) {
+      throw new Error('data load 실패');
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return isLoading;
+};
+
+export default useGetData;
