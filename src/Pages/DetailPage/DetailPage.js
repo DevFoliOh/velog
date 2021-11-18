3a56e557fc21d895166e75afd68e348edf27395c
Songchangyeop <waitwait0301@naver.com>
2021-11-19 00:27:57 +0900

    feat: detail page 컴포넌트에 데이터 받아와 렌더링 및 댓글 작성시 post 되는 onTextSubmit 함수 구현

--- src/Pages/DetailPage/DetailPage.js
+++ src/Pages/DetailPage/DetailPage.js
@@ -1,44 +1,56 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import Header from 'Components/Header/Header';
 import { style } from './DetailPageStyle';
-import useGetDetailData from 'Hooks/useGetDetailData';
+import useGetData from 'Hooks/useGetData';
 import Tag from 'Components/Tag/Tag';
 import Comment from 'Components/Comment/Comment';
-const DetailPage = () => {
-  // const [detailData, setDetailData] = useState({});
-  // const setData = useCallback((data) => {
-  //   setDetailData(data);
-  // }, []);
+import parse from 'html-react-parser';
 
-  // const loading = useGetDetailData(setData, id);
+const DetailPage = () => {
+  const [detailData, setDetailData] = useState({
+    tags: [],
+    title: '',
+    body: '',
+    createdAt: '',
+    updatedAt: '',
+    id: '',
+  });
+  const [commentData, setCommentData] = useState({});
+  const [tagArr, setTagArr] = useState([]);
+
+  const textRef = useRef();
+
+  const setPostData = useCallback((data) => {
+    setDetailData(data);
+  }, []);
+
+  const setComment = useCallback((data) => {
+    setCommentData(data);
+  }, []);
+
+  const loading = useGetData(setPostData, setComment);
+
+  const onTextSubmit = (event) => {
+    event.preventDefault();
+    console.log(textRef.current.value);
+  };
+
+  useEffect(() => {
+    detailData && setTagArr(detailData.tags);
+  }, [detailData]);
+
   return (
     <div>
       <Header></Header>
       <Main>
-        <Title>Title</Title>
+        <Title>{detailData.title}</Title>
         <TagList>
-          <Tag />
+          {tagArr &&
+            tagArr.map((tagContent) => <Tag tagContent={tagContent} />)}
         </TagList>
-        <Content>
-          이런 식으로 태그가 가지고 있는 의미에 맞게 사용하는 것인데, 이런 점
-          이외에도 CSS 스타일을 명시하는 태그를 사용하지 않는 것 또한 시맨틱
-          마크업의 한 종류 입니다. 즉, 태그가 가지는 의미 자체가 스타일이라면
-          이는 마크업 자체가 스타일을 갖는 것이기 때문에 시맨틱 마크업에
-          적합하지 않습니다. 예를 들어보겠습니다, 효과를 부여하는 과 태그가
-          있습니다. 둘은 동일하게 글자색을 진하게 하지만 태그의 경우는 그 자체가
-          "blod"의 약어이기 때문에 태그 자체가 스타일을 가진다고 할 수 있습니다.
-          하지만 의 경우는 "그 안의 내용이 다른 내용보다 더 가종되어야
-          한다."라는 의미를 가지고 있기 때문에 시맨틱 마크업에 더 적합합니다.
-          이런 식으로 태그가 가지고 있는 의미에 맞게 사용하는 것인데, 이런 점
-          이외에도 CSS 스타일을 명시하는 태그를 사용하지 않는 것 또한 시맨틱
-          마크업의 한 종류 입니다. 즉, 태그가 가지는 의미 자체가 스타일이라면
-          이는 마크업 자체가 스타일을 갖는 것이기 때문에 시맨틱 마크업에
-          적합하지 않습니다. 예를 들어보겠습니다, 효과를 부여하는 과 태그가
-          있습니다. 둘은 동일하게 글자색을 진하게 하지만 태그의 경우는 그 자체가
-          "blod"의 약어이기 때문에 태그 자체가 스타일을 가진다고 할 수 있습니다.
-          하지만 의 경우는 "그 안의 내용이 다른 내용보다 더 가종되어야
-          한다."라는 의미를 가지고 있기 때문에 시맨틱 마크업에 더 적합합니다.
-        </Content>
+        <Content
+          dangerouslySetInnerHTML={{ __html: parse(detailData.body) }}
+        ></Content>
         <UserContainer>
           <UserImg src="avatar.png" />
           <UserDescriptionWrap>
@@ -48,10 +60,15 @@ const DetailPage = () => {
         </UserContainer>
         <CommentContainer>
           <CommentCount>N개의 댓글</CommentCount>
-          <CommentTextArea placeholder="댓글을 작성하세요"></CommentTextArea>
-          <ButtonWraper>
-            <Button>댓글 작성</Button>
-          </ButtonWraper>
+          <form onSubmit={onTextSubmit}>
+            <CommentTextArea
+              placeholder="댓글을 작성하세요"
+              ref={textRef}
+            ></CommentTextArea>
+            <ButtonWraper>
+              <Button>댓글 작성</Button>
+            </ButtonWraper>
+          </form>
           <CommentList>
             <Comment></Comment>
           </CommentList>
